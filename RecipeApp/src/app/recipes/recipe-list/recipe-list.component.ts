e734d4ebbac1c4205a28b4d8e9182071028dd4b3
Wassim Ajjali <wassim.ajjali@lgesa.local>
2020-01-10 16:06:52 +0100

    display recipe list

--- RecipeApp/src/app/recipes/recipe-list/recipe-list.component.ts
+++ RecipeApp/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,12 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-
+import{Recipe} from '../recipe.model';
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes = [];
+  recipes: Recipe[] = [
+    new Recipe('AAAAAAAA','Paris','https://image.shutterstock.com/image-photo/hot-delicious-pepperoni-pizza-on-600w-1575681667.jpg'),
+    new Recipe('BBBBBBBB','Hawaii','https://image.shutterstock.com/image-photo/hot-delicious-pepperoni-pizza-on-600w-1575681667.jpg'),
+
+    new Recipe('CCCCCCCC','NY','https://image.shutterstock.com/image-photo/hot-delicious-pepperoni-pizza-on-600w-1575681667.jpg')
+
+   ];
   constructor() { }
 
   ngOnInit() {
--- RecipeApp/src/app/recipes/recipe.model.ts
+++ RecipeApp/src/app/recipes/recipe.model.ts
@@ -1,12 +1,12 @@
 export class Recipe {
 
     public name: string;
-    public desciption: string;
+    public description: string;
     public imagePath: string;
 
     constructor(name: string, desc: string, imagePath: string){
         this.name=name;
-        this.desciption=desc;
+        this.description=desc;
         this.imagePath=imagePath;
 
     }
